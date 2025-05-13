5c3e222e33790d5ca420ee021e9faf72d48f007f
ILYESS BASSOUMI <156471600+bassoumi@users.noreply.github.com>
2025-05-13 15:42:00 +0100

    Initial clean commit of e-commer-app

---
+++ serveces/config-server/src/test/java/com/elyes/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elyes.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
